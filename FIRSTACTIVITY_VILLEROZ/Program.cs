ce4852dc212ab2be2d728783b81a927e751178f9
ville <ville@DESKTOP-QGR04B9>
2023-02-09 17:47:56 +0800

    Add project files.

---
+++ FIRSTACTIVITY_VILLEROZ/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Divisblebyfive
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int first = 1;
+
+            Console.WriteLine("======================================");
+            Console.WriteLine("  Display Odd numbers divisible by 5");
+            Console.WriteLine("======================================");
+
+
+            for (first = 1; first <= 300; first++)
+            {
+                if (first % 5 == 0)
+                {
+                    Console.WriteLine(first);
+
+                }
+                first++;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
